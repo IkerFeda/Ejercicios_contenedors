e12c18767d4c41bec6cf733b3e626a1029df834c
IkerFeda <iferndac@myuax.com>
2023-11-29 18:37:54 +0100

    .

--- valarray.cpp
+++ valarray.cpp
@@ -1,1 +1,37 @@
+#include "valarray"
+#include "iostream"
+
+int main(){
+    std::valarray<int> numeros{10,56,7,45,3,2,100};
+    std::valarray<int> suma =numeros+1;
+    std::valarray<int> resta =numeros-1;
+    std::valarray<int> multiplicacion =numeros*2;
+    std::valarray<int> division =numeros/2;
+
+    std::cout << "Suma: ";
+    for (auto num : suma) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Resta: ";
+    for (auto num : resta) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Multiplicación: ";
+    for (auto num : multiplicacion) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "División: ";
+    for (auto num : division) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+
+    return 0;
+}
 
