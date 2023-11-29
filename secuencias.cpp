05088766d6f6e326878681d997fe90efb0942e97
IkerFeda <iferndac@myuax.com>
2023-11-29 18:09:54 +0100

    .

--- secuencias.cpp
+++ secuencias.cpp
@@ -1,3 +1,18 @@
-//
-// Created by ikerf on 29/11/2023.
-//
+#include "iostream"
+#include "list"
+#include "stack"
+
+int main (){
+   std::list<double> numeros= {5, 10, 15};
+   std::stack<double, std::list<double>> stack(numeros);
+   std::cout << "Empezamos a apilar" << std::endl;
+   stack.push(20);
+   stack.push(25);
+   std::cout << "El ultimo elemento es: " << stack.top() << std::endl;
+   std::cout << "Desapilamos" << std::endl;
+   stack.pop();
+   std::cout << "El ultimo elemento es: " << stack.top() << std::endl;
+   std::cout << "Desapilamos" << std::endl;
+   stack.pop();
+
+}
