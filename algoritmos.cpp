dfb5d76866039666118b6541cbdd43edc3e8185b
IkerFeda <iferndac@myuax.com>
2023-11-29 18:18:10 +0100

    .

--- algoritmos.cpp
+++ algoritmos.cpp
@@ -1,3 +1,20 @@
-//
-// Created by ikerf on 29/11/2023.
-//
+#include "iostream"
+#include "algorithm"
+#include "vector"
+
+int main (){
+
+    std::vector<int> numeros{10,56,7,45,3,2,100};
+    std::sort(numeros.begin(), numeros.end());
+    auto maximo = std::max_element(numeros.begin(), numeros.end());
+
+    std::cout << "Vector ordenado: ";
+    for (const auto& numero : numeros) {
+        std::cout << numero << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Elemento mas grande: " << *maximo << std::endl;
+
+    return 0;
+}
